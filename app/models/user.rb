ed5e1b15c9cce353f65d18e6089ee8060dc8580b
sakitakayanagi <melomelo.mymelody.sanrio@gmail.com>
2022-05-27 19:13:57 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, presence: true
+  validates :last_name,            format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :last_name_kana,       format: {with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name,           format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_kana,      format: {with: /\A[ァ-ヶー－]+\z/ }
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
 end
