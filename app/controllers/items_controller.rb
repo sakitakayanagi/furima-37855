3eae6f0f5362c6cdc63bb3086c2cf3ff1dde230e
sakitakayanagi <melomelo.mymelody.sanrio@gmail.com>
2022-05-28 20:13:27 +0900

    商品を登録できるようにするときの手順

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,27 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
-    # @items = Item.all 
+    @items = Item.all 
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_explain, :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, :item_price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :item_prefecture
+  belongs_to :item_scheduled_delivery
+  validates :item_category_id, :item_sales_status_id, :item_shipping_fee_status_id, :item_prefecture_id, :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
