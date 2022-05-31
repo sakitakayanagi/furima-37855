e3084a007e1bf0f224eda05473cadda821429c2c
sakitakayanagi <melomelo.mymelody.sanrio@gmail.com>
2022-05-31 16:35:07 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -35,6 +35,13 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+     end
+      redirect_to root_path
   end
 
   private
